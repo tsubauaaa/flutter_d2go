d68bc28b4906f2c50eda34729e6b4257d1b2a29c
ts-hirota <ts-hirota@kddi.com>
2021-10-18 23:02:32 +0900

    run test for loadModel

--- test/flutter_d2go_test.dart
+++ test/flutter_d2go_test.dart
@@ -1,14 +1,28 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_d2go/flutter_d2go.dart';
+import 'package:flutter_test/flutter_test.dart';
 
+/// Run as root in the flutter_d2go project
+/// "flutter test" to run
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   const MethodChannel channel = MethodChannel('tsubauaaa.com/flutter_d2go');
 
   final List<MethodCall> log = <MethodCall>[];
+
+  final Directory current = Directory.current;
+
+  setUpAll(() {
+    // expose path_provider
+    const MethodChannel path_provider_channel =
+        MethodChannel('plugins.flutter.io/path_provider');
+    path_provider_channel
+        .setMockMethodCallHandler((MethodCall methodCall) async {
+      return ".";
+    });
+  });
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
@@ -22,27 +36,29 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  // TODO: test loadModel method
-  // test('loadModel', () async {
-  //   await FlutterD2go.loadModel(
-  //     './example/assets/models/d2go.pt',
-  //     './example/assets/models/classes.txt',
-  //   );
-  //   expect(log, <Matcher>[
-  //     isMethodCall('loadModel', arguments: <String, dynamic>{
-  //       'modelPath': './example/assets/models/d2go.pt',
-  //       'labelPath': './example/assets/models/classes.txt',
-  //     })
-  //   ]);
-  // });
+  test('loadModel', () async {
+    await FlutterD2go.loadModel(
+      '${current.path}/example/assets/models/d2go.pt',
+      '${current.path}/example/assets/models/classes.txt',
+    );
+    expect(log, <Matcher>[
+      isMethodCall('loadModel', arguments: <String, dynamic>{
+        'absModelPath': '${current.path}/example/assets/models/d2go.pt',
+        'absLabelPath': '${current.path}/example/assets/models/classes.txt',
+        'assetModelPath': '${current.path}/example/assets/models/d2go.pt',
+        'assetLabelPath': '${current.path}/example/assets/models/classes.txt',
+      })
+    ]);
+  });
 
   test('getImagePrediction', () async {
     await FlutterD2go.getImagePrediction(
-      image: File('./example/assets/images/test1.png'),
+      image: File('${current.path}/example/assets/images/test1.png'),
     );
     expect(log, <Matcher>[
       isMethodCall('predictImage', arguments: <String, dynamic>{
-        'image': File('./example/assets/images/test1.png').readAsBytesSync(),
+        'image': File('${current.path}/example/assets/images/test1.png')
+            .readAsBytesSync(),
         'width': kInputWidth,
         'height': kInputHeight,
         'mean': kTorchvisionNormMeanRGB,
