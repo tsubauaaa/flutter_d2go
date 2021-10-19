84389d47c2a38251f5d79073b2bfa2a1bb856ec0
ts-hirota <ts-hirota@kddi.com>
2021-10-19 23:59:54 +0900

    ios under consideration

--- ios/Classes/SwiftFlutterD2goPlugin.swift
+++ ios/Classes/SwiftFlutterD2goPlugin.swift
@@ -3,12 +3,22 @@ import UIKit
 
 public class SwiftFlutterD2goPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_d2go", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "tsubauaaa.com/flutter_d2go", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterD2goPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    let args:Dictionary<String, AnyObject> = call.arguments as! Dictionary<String, AnyObject>;
+    // ここに書いていけばよいのでは？
+    if ("loadModel" == call.method) {
+            print(args)
+            result("iOS " + UIDevice.current.systemVersion)
+    } else if ("predictImage" == call.method) {
+        print(args)
+        result("iOS " + UIDevice.current.systemVersion)
+    }
+
     result("iOS " + UIDevice.current.systemVersion)
   }
 }
