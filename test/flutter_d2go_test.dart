a6e708fcc6bb1922840b876725f0294e7c7f2643
ts-hirota <ts-hirota@kddi.com>
2021-11-04 23:35:49 +0900

    wrote a test that works

--- test/flutter_d2go_test.dart
+++ test/flutter_d2go_test.dart
@@ -16,10 +16,9 @@ void main() {
 
   setUpAll(() {
     // expose path_provider
-    const MethodChannel path_provider_channel =
+    const MethodChannel pathProviderChannel =
         MethodChannel('plugins.flutter.io/path_provider');
-    path_provider_channel
-        .setMockMethodCallHandler((MethodCall methodCall) async {
+    pathProviderChannel.setMockMethodCallHandler((MethodCall methodCall) async {
       return ".";
     });
   });
@@ -27,20 +26,37 @@ void main() {
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
       log.add(methodCall);
-      return null;
+      if (methodCall.method == 'loadModel') {
+        return "success";
+      } else {
+        return [
+          {
+            'rect': {
+              'left': 74.65713500976562,
+              'top': 76.94147491455078,
+              'right': 350.64324951171875,
+              'bottom': 323.0279846191406
+            },
+            'confidenceInClass': 0.985002338886261,
+            'detectedClass': "bicycle",
+            'mask': [0, 255, 255, 0],
+          },
+        ];
+      }
     });
-    log.clear();
   });
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
+    log.clear();
   });
 
   test('loadModel', () async {
-    await FlutterD2go.loadModel(
+    final res = await FlutterD2go.loadModel(
       modelPath: '${current.path}/example/assets/models/d2go.pt',
       labelPath: '${current.path}/example/assets/models/classes.txt',
     );
+    expect(res, "success");
     expect(log, <Matcher>[
       isMethodCall('loadModel', arguments: <String, dynamic>{
         'absModelPath': '${current.path}/example/assets/models/d2go.pt',
@@ -52,9 +68,22 @@ void main() {
   });
 
   test('getImagePrediction', () async {
-    await FlutterD2go.getImagePrediction(
+    final res = await FlutterD2go.getImagePrediction(
       image: File('${current.path}/example/assets/images/test1.png'),
     );
+    expect(res, [
+      {
+        'rect': {
+          'left': 74.65713500976562,
+          'top': 76.94147491455078,
+          'right': 350.64324951171875,
+          'bottom': 323.0279846191406
+        },
+        'confidenceInClass': 0.985002338886261,
+        'detectedClass': "bicycle",
+        'mask': [0, 255, 255, 0],
+      },
+    ]);
     expect(log, <Matcher>[
       isMethodCall('predictImage', arguments: <String, dynamic>{
         'image': File('${current.path}/example/assets/images/test1.png')
